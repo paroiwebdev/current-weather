ac2a1b4f46d71d026ff19bcdd3940208b95a9649
Limon Paroi <34869737+paroiwebdev@users.noreply.github.com>
2019-04-25 02:22:55 +0600

    Update app.js

--- app.js
+++ app.js
@@ -13,13 +13,6 @@ app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// passport config
-app.use(require("express-session")({
-  secret: "This is Man Utd!",
-  resave: false,
-  saveUninitialized: false
-}));
-
 app.use("/", indexRoutes);
 
 app.listen(process.env.PORT || 4000,function(){
