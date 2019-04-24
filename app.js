a20dfb2ccf7e8b21a3f42e6a28b857af53d621c7
Limon Paroi <34869737+paroiwebdev@users.noreply.github.com>
2019-04-25 02:34:16 +0600

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const express       = require("express");
 const app           = express();
 const moment        = require('moment');
 const request       = require("request");
-const session       = require('express-session');
 const bodyParser    = require("body-parser");
-
-//requiring routes
-var indexRoutes   = require("./routes/index");
 
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//requiring routes
+var indexRoutes   = require("./routes/index");
 
 app.use("/", indexRoutes);
 
